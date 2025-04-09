e5d1b7a5887ca44ab4acf87e7ed89a5729d8ba10
tomson218 <tomasz.pstrowski20@gmail.com>
2025-04-09 20:19:25 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class NSD
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(82, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(79, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(277, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 142);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(228, 142);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 280);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class NSD : Form
+    { 
+         int i, x, y, z, r;
+
+        public NSD()
+        {
+            InitializeComponent();
+        }
+
+       /* private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }*/
+
+        static void Main()
+        {
+            int  x, y, z, r;
+            int i=0;
+
+            Console.WriteLine("Введіть значення х");
+        M2: try
+            {
+                x = Int32.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Значення х введено невірно!");
+                goto M2;
+            }
+        Console.WriteLine("Введіть значення y");
+    M4: try
+        {
+            y = Int32.Parse(Console.ReadLine());
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Значення y введено невірно!");
+            goto M4;
+        }
+if ((x >  0) & (x <  ( (2^32) -1 ) )) 
+  {
+                i=0;
+    if ((y >  0) & (y <  ( (2^32) -1 ) )) 
+      { 
+       if (x!=0) 
+         {
+if (y!=0) 
+  {
+    if (x > y)
+    {
+          r = x%y;
+          if (r == 0)
+          {
+              Console.WriteLine("Найбільший спільний дільник у = {0}", y);}
+              else
+               { M1: z=y-1 ;
+                   r=x%z;
+                   if (r == 0)
+                   {
+                       r = y % z;
+                       if (r == 0)
+                       {
+                           Console.WriteLine("Найбільший спільний дільник z  = ", z);
+                       }
+                           else goto M1;
+                   }
+                       else  goto M1;
+              }
+    }
+                       else  {
+                              i=0; 
+                              goto M2;
+                             }
+  }                            
+                              
+                      else { i=i+1; 
+                             if (i==3)
+                               {
+                                //M5: Console.WriteLine ("Кількість спроб вводу  досягла максимуму. Роботу завершено!");
+                               }
+                             else goto M4;
+                            }         
+}
+    else
+       {
+           i = i + 1; 
+           if (i == 3) goto M5; 
+           else goto M2;
+       }
+                 
+                   }
+                     else { Console.WriteLine("Введене значення y не належить до натуральних чисел!"); 
+                           i=i+1;
+                           if (i == 3) goto M5;
+                           else goto M4; 
+                          }
+               } 
+                 else { Console.WriteLine("Введене значенняя х не належить до натуральних чисел!"); 
+                       i=i+1; 
+                       if (i == 3) goto M5; 
+                       else goto M2;
+                      }
+M5: Console.WriteLine("Кількість спроб вводу  досягла максимуму. Роботу завершено!");
+          }
+    }
+}
+
+
+
